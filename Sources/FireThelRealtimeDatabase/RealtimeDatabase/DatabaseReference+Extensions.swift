78d37526f4d6119352d4006991d34bc3ee798ae2
vebbis321 <veb.dan@hotmail.com>
2023-08-21 16:35:28 +0200

    fix: childNode listener in RTDB to return a single value

--- Sources/FireThelRealtimeDatabase/RealtimeDatabase/DatabaseReference+Extensions.swift
+++ Sources/FireThelRealtimeDatabase/RealtimeDatabase/DatabaseReference+Extensions.swift
@@ -39,7 +39,7 @@ extension DatabaseReference {
     public func observeChildNode<T: Codable>(
         path: Path,
         event: DataEventType
-    ) -> AnyPublisher<[T], Error> {
+    ) -> AnyPublisher<T, Error> {
         let ref = self.child(path)
         return Publishers.ObserveNodePublisher(ref: ref, event: event).eraseToAnyPublisher()
     }
